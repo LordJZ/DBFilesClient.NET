74cf997ee91d9c23f33094b1d9c7dd290b4c2dba
LordJZ <a553r7fa1l3d@gmail.com>
2012-05-01 16:31:22 +0400

    Implement IStorage.GetEntryPtr

---
+++ DBFilesClient.NET/IStorageExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DBFilesClient.NET
+{
+    public static class IStorageExtensions
+    {
+        public static unsafe void* GetEntryPtr(this IStorage storage, uint id)
+        {
+            return storage.GetEntry(id).ToPointer();
+        }
+    }
+}
